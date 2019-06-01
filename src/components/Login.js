6afc8217dbdab9294ea5fee9cc506dedba40116b
mpan <mattijaakkopanu@gmail.com>
2019-06-01 14:03:05 +0300

    simple server and login component

---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Login(props) {
+    return (
+        <div>
+            <p>Give username:</p>
+            <input />
+            <button>Button</button>
+        </div>
+    );
+}
