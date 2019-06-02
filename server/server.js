a3c50eb293f48b4aab7db02e23c7818a463d9607
mpan <mattijaakkopanu@gmail.com>
2019-06-02 23:04:55 +0300

    login using server works

--- server/server.js
+++ server/server.js
@@ -1,7 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require('cors');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const port = 3001;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// https://codeforgeek.com/handle-get-post-request-express-4/
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.json({username: "username"}));
+
+app.post('/', function (req, res) {
+    var username = req.body.username;
+    console.log("User name = " + username);
+    res.send({username: username, success: true});
+    res.end("yes");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Login from './Login';
+import fetch from 'cross-fetch';
 
 class Root extends React.Component {
 
@@ -14,7 +15,7 @@ class Root extends React.Component {
         if (this.state.username === '') {
             return (
                 <div>{this.state.username}
-                    <Login onSubmitUsername={this.setUsername}/>
+                    <Login onSubmitUsername={this.setUsername} />
                 </div>
             );
         } else return (
@@ -25,8 +26,32 @@ class Root extends React.Component {
     }
 
     setUsername = (name) => {
-        this.setState({...this.state, username: name});
+        postData('http://localhost:3001', { username: name })
+            .then(data => {
+                this.setState({ ...this.state, username: name });
+                console.log(JSON.stringify(data));
+            }) // JSON-string from `response.json()` call
+            .catch(error => console.error(error));
     }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+function postData(url = '', data = {}) {
+    // Default options are marked with *
+    return fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, cors, *same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+        .then(response => response.json()); // parses JSON response into native Javascript objects 
 }
 
 export default Root;
