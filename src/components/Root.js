e3b7ad4a4fe6e0d8e3b01f6076500ce32b1c2fec
mpan <matti.panula@cimcorp.com>
2019-06-04 15:31:53 +0300

    cleanup

--- src/components/Root.js
+++ src/components/Root.js
@@ -6,36 +6,36 @@ import { postData } from '../utils.js';
 
 class Root extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: ''
+    };
+  }
 
-    render() {
-        if (this.state.username === '') {
-            return (
-                <Input className="login" 
-                       buttonText='Login' 
-                       onSubmitUsername={this.setUsername} />
-            );
-        } else return (
-            <div>
-                <MessageList messages={this.state.messages}/>
-                <Input className="message" 
-                        buttonText='Send' 
-                        onSubmitUsername={console.log('todo, msg send')} />
-            </div>
-        );
-    }
+  render() {
+    if (this.state.username === '') {
+      return (
+        <Input className="login"
+          buttonText='Login'
+          onSubmitUsername={this.doLogin} />
+      );
+    } else return (
+      <div>
+        <MessageList messages={this.state.messages} />
+        <Input className="message"
+          buttonText='Send'
+          onSubmitUsername={console.log('todo, msg send')} />
+      </div>
+    );
+  }
 
-    setUsername = (name) => {
-        postData('http://localhost:3001', { username: name })
-            .then(data => {
-                this.setState({ ...this.state, ...data });                
-            })
-            .catch(error => console.error(error));
-    }
+  doLogin = (name) => {
+    postData('http://localhost:3001', { username: name })
+      .then(data => {
+        this.setState({ ...this.state, ...data });
+      })
+      .catch(error => console.error(error));
+  }
 }
 export default Root;
